1b15fdb44b3a2c89e11c753b8ec8efacf8ce0fb8
raknair <rakesh.nair@accelya.com>
2021-04-18 19:41:02 +0530

    first commit

---
+++ src/main/java/org/spring/microservices/PhotoAppWebApplication.java
@@ -0,0 +1,13 @@
+package org.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppWebApplication.class, args);
+	}
+
+}
