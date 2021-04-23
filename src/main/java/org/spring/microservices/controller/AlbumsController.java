3b615d78e33276629a53baf2eb9499288e846b2f
raknair <rakesh.nair@accelya.com>
2021-04-23 21:20:32 +0530

    Fetch Access Token

--- src/main/java/org/spring/microservices/controller/AlbumsController.java
+++ src/main/java/org/spring/microservices/controller/AlbumsController.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.spring.microservices.response.AlbumRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -13,10 +19,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AlbumsController {
+	@Autowired
+	OAuth2AuthorizedClientService service;
 
 	@GetMapping("/albums")
 	public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
-		System.out.println("Principal " + principal);
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+		OAuth2AuthorizedClient oauth2client = service.loadAuthorizedClient(token.getAuthorizedClientRegistrationId(),
+				token.getName());
+		String jwtAccessToken = oauth2client.getAccessToken().getTokenValue();
+		System.out.println("Access token " + jwtAccessToken);
 		OidcIdToken idToken = principal.getIdToken();
 		String idTokenVal = idToken.getTokenValue();
 		System.out.println("Id token " + idTokenVal);
